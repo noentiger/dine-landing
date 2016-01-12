dbc719f091d2082ea2c90ed6452e992518e5006d
Kim Wijk <kim.wijk@gmail.com>
2016-01-12 01:42:10 +0100

    Add layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,51 @@
-console.log('Hello World')
+var clickEvent = ((document.ontouchstart !== null) ? 'click' : 'touchstart');
+
+document.getElementById('signup')
+    .addEventListener('submit', signUp);
+
+document.getElementById('resetForm')
+    .addEventListener(clickEvent, resetForm);
+
+function resetForm() {
+    hide('signupDone');	
+    show('signup');
+    document.getElementById('send').disabled = false;
+    document.getElementById('email').value = '';
+}
+
+function signUp(e) {
+
+    e.preventDefault();
+
+    // Perform optimistic action
+    hide('signup');
+    show('signupDone');
+
+    document.getElementById('send').disabled = true;
+    var userEmail = document.getElementById('email').value;
+
+    Parse.initialize(
+        'Fh0rneGUGVuUjxCckhx1vGs2mHpFMuEGWNUzvxlO', 
+        'xLZnitYsGrTc7sD2gp1nwJj47bLxrPeeOhAof771'
+    );
+    
+    var Users = Parse.Object.extend('Users');
+    var Users = new Users();
+      Users.save({email: userEmail}, {
+      success: function(object) {
+        // Saved
+      },
+      error: function(model, error) {
+        console.log('model', model);
+        console.log('error', error);
+      }
+    });
+}
+
+function show(target) {
+    document.getElementById(target).style.display = 'block';
+}
+
+function hide(target) {
+    document.getElementById(target).style.display = 'none';
+}
